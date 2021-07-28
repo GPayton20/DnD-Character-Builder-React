dfd5553a6c4bb9ef03cc92a5393bc06c9b347117
Geoffrey <geoffrpayton@gmail.com>
2021-07-28 17:03:39 -0400

    redo last change

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,30 @@ import { useState, useEffect } from 'react';
 
 function App() {
 
+  const [playerClass, setPlayerClass] = useState(null)
 
+  useEffect(() => {
+    const api = async () => {
+      const response = await fetch(`https://www.dnd5eapi.co/api/classes/fighter`);
+      const data = await response.json();
+      console.log(data)
+      setPlayerClass(data);
+    }
+
+    api();
+  }, [])
   
   return (
     <div className="App">
+      {playerClass
+      ? playerClass.proficiencies.map(prof => {
+        return (
+          <ul>
+            <li key={prof.index}>{prof.name}</li>
+          </ul>
+        )
+      })
+      : null}
     </div>
   );
 }
